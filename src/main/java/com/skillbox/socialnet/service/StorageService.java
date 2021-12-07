06142b903923990c2e07b7a988a04c6b60467e2c
JevaiNick <62241059+JevaiNick@users.noreply.github.com>
2021-12-08 00:48:54 +0200

    img works

--- src/main/java/com/skillbox/socialnet/service/StorageService.java
+++ src/main/java/com/skillbox/socialnet/service/StorageService.java
@@ -12,14 +12,13 @@ import com.skillbox.socialnet.model.RS.DefaultRS;
 import com.skillbox.socialnet.model.dto.FileDTO;
 import com.skillbox.socialnet.model.entity.Person;
 import com.skillbox.socialnet.model.mapper.DefaultRSMapper;
+import com.skillbox.socialnet.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import net.bytebuddy.utility.RandomString;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
@@ -28,12 +27,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StorageService {
 
-    private AuthService authService;
+    private final AuthService authService;
+    private final PersonRepository personRepository;
 
     public DefaultRS<?> saveImageToProfile(String type, MultipartFile multipartFile) throws IOException {
         Person activePerson = authService.getPersonFromSecurityContext();
-        File file = new File("file.jpg");
-        multipartFile.transferTo(file);
+        File file = new File("src/main/resources/targetFile.jpg");
+
+        try (OutputStream os = new FileOutputStream(file)) {
+            os.write(multipartFile.getBytes());
+        }
+
         FileDTO fileDTO = new FileDTO();
         if (!multipartFile.isEmpty()) {
             String prevImg = "";
@@ -41,7 +45,8 @@ public class StorageService {
                 String[] strings = activePerson.getPhoto().split("/");
                 prevImg = strings[strings.length - 1];
             }
-            activePerson.setPhoto(savePhotoInCloud(file, type, prevImg));
+            activePerson.setPhoto(savePhotoInCloud(file, prevImg));
+            personRepository.save(activePerson);
             fileDTO.setOwnerId(activePerson.getId());
             fileDTO.setFileName(multipartFile.getName());
             fileDTO.setRelativeFilePath(activePerson.getPhoto());
@@ -53,11 +58,11 @@ public class StorageService {
         return DefaultRSMapper.of(fileDTO);
     }
 
-    private String savePhotoInCloud(File file, String type, String prevImg) {
+    private String savePhotoInCloud(File file, String prevImg) {
         String fileName = System.currentTimeMillis() + file.getName();
         Regions clientRegion = Regions.EU_CENTRAL_1;
         String bucketName = "jevaibucket/publicprefix";
-        String fileObjKeyName = fileName + type;
+        String fileObjKeyName = fileName;
         AWSCredentials awsCredentials =
                 new BasicAWSCredentials("AKIAVAR2I7GKLP66SIHL", "W3dXfLlwvfj+E8ucH62wwgalYZufOXLwFx2yxWu+");
         AmazonS3 s3Client = AmazonS3ClientBuilder
@@ -66,9 +71,22 @@ public class StorageService {
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
         s3Client.putObject(new PutObjectRequest(bucketName, fileObjKeyName, file));
-        if (prevImg.isEmpty()){
+        if (!prevImg.isEmpty()){
             s3Client.deleteObject(new DeleteObjectRequest(bucketName, prevImg));
         }
         return "https://jevaibucket.s3.eu-central-1.amazonaws.com/publicprefix/" + fileObjKeyName;
+    }
+
+    private void deletePhotoFromCloud(String prevImg){
+        Regions clientRegion = Regions.EU_CENTRAL_1;
+        String bucketName = "jevaibucket/publicprefix";
+        AWSCredentials awsCredentials =
+                new BasicAWSCredentials("AKIAVAR2I7GKLP66SIHL", "W3dXfLlwvfj+E8ucH62wwgalYZufOXLwFx2yxWu+");
+        AmazonS3 s3Client = AmazonS3ClientBuilder
+                .standard()
+                .withRegion(clientRegion)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+        s3Client.deleteObject(new DeleteObjectRequest(bucketName, prevImg));
     }
 }
