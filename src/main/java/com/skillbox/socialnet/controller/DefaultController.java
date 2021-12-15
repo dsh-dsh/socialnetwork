c210c20928a3d36da10de3f698e6dceb06bc35c5
Alex <kochurov92@mail.ru>
2021-12-15 21:45:50 +0500

    add new front

--- src/main/java/com/skillbox/socialnet/controller/DefaultController.java
+++ src/main/java/com/skillbox/socialnet/controller/DefaultController.java
@@ -1,33 +1,33 @@
-//package com.skillbox.socialnet.controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//@CrossOrigin
-//@Controller
-//public class DefaultController {
-//
-//
-//    @RequestMapping
-//    public String index() {
-//        return "index";
-//    }
-//
-//    @GetMapping("/change-password")
-//    public String changePassword() {
-//        return "index";
-//    }
-//
-//    @GetMapping("/shift-email")
-//    public String shiftEmail() {
-//        return "index";
-//    }
-//
-//    @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET}, value = "/**/{path:[^\\\\.]*}")
-//    public String redirectToIndex() {
-//    return "forward:/";
-//}
-//
-//}
+package com.skillbox.socialnet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@CrossOrigin
+@Controller
+public class DefaultController {
+
+
+    @RequestMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/change-password")
+    public String changePassword() {
+        return "index";
+    }
+
+    @GetMapping("/shift-email")
+    public String shiftEmail() {
+        return "index";
+    }
+
+    @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET}, value = "/**/{path:[^\\\\.]*}")
+    public String redirectToIndex() {
+    return "forward:/";
+}
+
+}
