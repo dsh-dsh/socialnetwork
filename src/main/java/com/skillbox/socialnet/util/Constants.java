777cd018363abcd560d074578c6e90171729ef77
dsh-dsh <dan.shipilov@gmail.com>
2021-12-31 16:39:15 +0300

    add additionalPosts to feeds

--- src/main/java/com/skillbox/socialnet/util/Constants.java
+++ src/main/java/com/skillbox/socialnet/util/Constants.java
@@ -30,5 +30,6 @@ public class Constants {
     public static final String API_ACCOUNT = "/api/v1/account";
     public static final String API_PLATFORM = "/api/v1/platform";
     public static final int RECOMMENDED_FRIENDS_LIMIT = 10;
+    public static final int RECOMMENDED_POST_LIMIT = 10;
 
 }
