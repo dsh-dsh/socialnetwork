20d3593b64b757b5ea59ef081d3cfceaf51e0197
JevaiNick <62241059+JevaiNick@users.noreply.github.com>
2022-01-25 17:01:12 +0200

    edit changelog

--- src/main/resources/db/changelog/v0.0.05_insertPost2Tags.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO post2tag
-(post_id, tag_id)
-VALUES
-(1, 1),
-(1, 2),
-(2, 3),
-(2, 2),
-(3, 1),
-(3, 3);
--- src/main/resources/db/changelog/v0.0.9_addSeenColumnAndTestData.sql
+++ src/main/resources/db/changelog/v0.0.9_addSeenColumnAndTestData.sql
@@ -4,7 +4,3 @@
 ALTER TABLE notification
 ADD COLUMN IF NOT EXISTS seen boolean;
 
-INSERT INTO notification
-(type_id, sent_time, person_id, entity_id, contact, seen)
-VALUES
-(1, '2021-11-12 14:55:56', 2, '2', 'p2@mail.ru', false)
