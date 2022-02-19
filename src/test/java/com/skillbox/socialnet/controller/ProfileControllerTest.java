0a57ec03e446429279153cfdc33371dcf93b3433
dsh-dsh <dan.shipilov@gmail.com>
2022-02-19 09:49:44 +0300

    refactoring block/unblock user in Test

--- src/test/java/com/skillbox/socialnet/controller/ProfileControllerTest.java
+++ src/test/java/com/skillbox/socialnet/controller/ProfileControllerTest.java
@@ -1,8 +1,14 @@
 package com.skillbox.socialnet.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skillbox.socialnet.model.entity.Friendship;
+import com.skillbox.socialnet.model.entity.Person;
+import com.skillbox.socialnet.model.enums.FriendshipStatusCode;
 import com.skillbox.socialnet.model.rq.PostChangeRQ;
 import com.skillbox.socialnet.model.rq.UserChangeRQ;
+import com.skillbox.socialnet.repository.FriendshipRepository;
+import com.skillbox.socialnet.service.FriendsService;
+import com.skillbox.socialnet.service.PersonService;
 import com.skillbox.socialnet.util.Constants;
 import lombok.extern.java.Log;
 import org.junit.jupiter.api.Test;
@@ -19,7 +25,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -38,11 +46,18 @@ class ProfileControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private FriendshipRepository friendshipRepository;
+    @Autowired
+    private PersonService personService;
+    @Autowired
+    private FriendsService friendsService;
 
 
     private static final String URL_PREFIX = "/api/v1/users/";
     private static final String ME = "me";
     private static final int ID_1 = 1;
+    private static final int ID_2 = 1;
     private static final String BLOCK = "block";
     private static final String EXISTING_EMAIL = "nikita.r200019@gmail.com";
     private static final String P1_MAIL = "p1@mail.ru";
@@ -243,13 +258,15 @@ class ProfileControllerTest {
 
     @Test
     @WithUserDetails(P1_MAIL)
-    @Sql(value = "/sql/person/unblockP1.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(value = "/sql/person/unblockP1.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+//    @Sql(value = "/sql/person/unblockP1.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//    @Sql(value = "/sql/person/unblockP1.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void blockPerson() throws Exception {
-        mockMvc.perform(put(URL_PREFIX + BLOCK + "/" + ID_1))
+        mockMvc.perform(put(URL_PREFIX + BLOCK + "/" + ID_2))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.message").value("ok"));
+
+        assertEquals(1, getBlockedRelationNumber(ID_1, ID_2));
     }
 
     @Test
@@ -261,13 +278,15 @@ class ProfileControllerTest {
 
     @Test
     @WithUserDetails(P1_MAIL)
-    @Sql(value = "/sql/person/blockP1.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(value = "/sql/person/unblockP1.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+//    @Sql(value = "/sql/person/blockP1.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//    @Sql(value = "/sql/person/unblockP1.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void unblockPerson() throws Exception {
-        mockMvc.perform(delete(URL_PREFIX + BLOCK + "/" + ID_1))
+        mockMvc.perform(delete(URL_PREFIX + BLOCK + "/" + ID_2))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.message").value("ok"));
+
+        assertEquals(0, getBlockedRelationNumber(ID_1, ID_2));
     }
 
     @Test
@@ -303,6 +322,15 @@ class ProfileControllerTest {
         postChangeRQ.setTitle(title);
         postChangeRQ.setTags(new ArrayList<>());
         return postChangeRQ;
+    }
+
+
+
+    private int getBlockedRelationNumber(int srcPersonId, int dstPersonId) {
+        Person srcPerson = personService.getPersonById(srcPersonId);
+        Person dstPerson = personService.getPersonById(dstPersonId);
+        List<Friendship> requests = friendshipRepository.findRequests(srcPerson, dstPerson, FriendshipStatusCode.BLOCKED);
+        return requests.size();
     }
 
 }
